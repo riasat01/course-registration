c66088b6a7b7b99f5652cf034316a02712802acb
unknown <riasat01@outlook.com>
2023-09-16 14:53:46 +0600

    prop-types and react icons installed, json data loaded

--- src/component/course/Course.jsx
+++ src/component/course/Course.jsx
@@ -1,9 +1,31 @@
-const Course = () => {
+import PropTypes from 'prop-types';
+import {FiDollarSign} from 'react-icons/fi'
+import {BsBook} from 'react-icons/bs'
+
+const Course = ({course}) => {
+    const {title, description, price, credit_hour, image} = course;
     return (
         <div>
-            
+            <img src={image} alt="" />
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <div>
+                <div>
+                <FiDollarSign></FiDollarSign>
+                <p>Pirce : {price}</p>
+                </div>
+                <div>
+                    <BsBook></BsBook>
+                    <p>Credit : {credit_hour}hr</p>
+                </div>
+            </div>
+            <button>Select</button>
         </div>
     );
 };
+
+Course.propTypes = {
+    course: PropTypes.object.isRequired
+}
 
 export default Course;
--- src/component/courses/Courses.jsx
+++ src/component/courses/Courses.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Course from "../course/Course";
+
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetch('course.json')
+        .then(res => res.json())
+        .then(data => setCourses(data));
+    }, [])
     return (
         <div>
-            
+            {
+                courses.map(course => <Course key={course.id} course={course}></Course>)
+            }
         </div>
     );
 };
