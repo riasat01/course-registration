6a44d8deaca355586768e73b5a15987cbff99cc0
unknown <riasat01@outlook.com>
2023-09-16 14:28:04 +0600

    tailwind setup done, json fake data added, basic file structure created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Courses from './component/courses/Courses'
+
+function App() {
+
+  return (
+    <>
+      <h1>Course Registration</h1>
+      <Courses></Courses>
+    </>
+  )
+}
+
+export default App
